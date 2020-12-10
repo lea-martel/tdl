fa145371e68a9830fe1de2af8794a5ad570fe2dd
lea-martel <lea.martel@laplateforme.io>
2020-12-10 17:15:18 +0100

    disconnect ok

--- header.php
+++ header.php
@@ -1,5 +1,8 @@
 
-<?php include 'src/Base.php'; ?>
+<?php include 'src/Base.php';
+$login = new Base\register_connexion();
+?>
+
 <head>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="styles/style.css">
@@ -8,5 +11,10 @@
 <header>
     <div class="container">
         <h1 class="title">TO DO LIST</h1>
+        <?php if (isset($_SESSION['id'])) { ?>
+        <nav class="nav">
+            <h5 class="text-white"><a class="lien" href="disconnect.php">Déconnexion</a></h5>
+          <?php  } ?>
+        </nav>
     </div>
 </header>
--- todolist.php
+++ todolist.php
@@ -6,6 +6,7 @@
     <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
     <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://kit.fontawesome.com/5a25ce672a.js" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
             integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
             crossorigin="anonymous"></script>
@@ -58,7 +59,6 @@ $dataTime = new DateTime();
                 </div>
             <?php } ?>
         </div>
-
     </div>
 </main>
 <?php include 'footer.php' ?>
@@ -83,6 +83,7 @@ $dataTime = new DateTime();
         html += '</div>';
         return html + '</div>' + '</div>';
     }
+
     $(document).ready(function(){
         callback();
     });
